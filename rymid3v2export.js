8ca69cfa4f457611d8c6269dd1653f62c4b7f5af
AJBeahm <alexanderbeahm@gmail.com>
2024-01-29 11:01:48 -0600

    Add instructions for dev.

--- rymid3v2export.js
+++ rymid3v2export.js
@@ -1,1 +1,46 @@
-//TODO Implement
+//https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Working_with_files
+//https://community.mp3tag.de/t/text-file-to-tag-conversion-tips/8299
+//https://docs.mp3tag.de/export/
+
+// On button/icon press...
+
+// Collect all items under these banners
+//<meta itemprop="url"
+//-  url stub
+
+//<table class="album_info">
+// - Title
+// - Artist
+// - Type
+// - Release Date
+// - Recorded
+// - Genres/subgenres
+// - Descriptors
+// - Language
+// - Common tags
+
+//<ul id="tracks_mobile">
+//- Tracks
+//  - Track number
+//  - Track title
+//  - Featured artist (default none)
+//  - Track length
+//- Total length
+
+//<div id="my_catalog">
+//- Tags
+//- Rating
+//- Review
+//- Track ratings
+
+//<ul id="credits_credits_mobile">
+//- Name
+//- Roles
+//  - Name
+//  - Track (default all)
+
+
+//Collect this information into a txt format with following format:
+//TODO: Figure out format, whole album info needs to be on each track.
+
+//Download file to folder. Error handling?
