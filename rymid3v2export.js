258df3a47df36ebe8a3008fb34e2749529797551
AJBeahm <alexanderbeahm@gmail.com>
2024-02-01 11:35:48 -0600

    Adding format string and some basic workflow steps.

--- rymid3v2export.js
+++ rymid3v2export.js
@@ -3,6 +3,61 @@
 //https://docs.mp3tag.de/export/
 
 // On button/icon press...
+function temp_display(str){
+    console.log(str);
+}
+
+function scrapeMeta(){
+    var metaTags = document.getElementsByTagName("meta");
+}
+
+function scrapeAlbumInfo(){
+    var albumInfo = document.getElementsByClassName("album_info");
+}
+
+function scrapeTracks(){
+    var tracks = document.getElementById("tracks_mobile");
+}
+
+function scrapeCatalog(){
+    var catalogInfo = document.getElementById("my_catalog");
+}
+
+function scrapeCredits(){
+    var credits = document.getElementById("credits_credits_mobile");
+}
+
+function pullData(){
+    scrapeMeta();
+    scrapeAlbumInfo();
+    scrapeTracks();
+    scrapeCatalog();
+    scrapeCredits();
+}
+
+function parseDict(dict){
+    //TODO Need to make some objects or dict that supports the multiple tracks across the spectrum of metadata.
+}
+
+function generateFile(text){
+    var blob = new Blob([text], {type: "text/plain"});
+    var url = URL.createObjectURL(blob);
+    var link = document.createElement("a");
+    link.href = url;
+    //Change filename to specific version config?
+    link.download = "id3v2_3.txt";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
+
+browser.action.onclicked.addListener((tab) => {
+    let title = browser.action.getTitle();
+    temp_display(title);
+    generateFile(parseDict(pullData()));
+})
+
 
 // Collect all items under these banners
 //<meta itemprop="url"
@@ -42,5 +97,7 @@
 
 //Collect this information into a txt format with following format:
 //TODO: Figure out format, whole album info needs to be on each track.
+//TODO: WOAS VS. WXXX for RYM url
+let formatString = "MVIN;;MVNM;;TALB;;TCMP;;TCOM;;TCON;;TDAT;;TEXT;;TIT2;;TLAN;;TLEN;;TOAL;;TORY;;TPE1;;TPE3;;TPUB;;TRCK;;TRDA;;TXXX;;TYER;;WOAS;;WXXX;;"
 
 //Download file to folder. Error handling?
